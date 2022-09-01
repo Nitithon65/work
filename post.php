8cadcfaa37adcbdb655300758764c7b081a20689
Palk101043 <s6403052412073@kmutnb.ac.th>
2022-09-01 14:43:37 +0700

    work

---
+++ post.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>post</title>
+</head>
+<body>
+    <h1 style="text-align: center;">Webboard KakKak</h1>
+        <hr>
+        <center>ต้องการดูกระทู้หมายเลข = <?php echo $_GET["id"];?><br></center><br>
+        <table style="border: 2px solid black; width:40%" align ="center">
+          <tr><td colspan="2" style="background-color: #6CD2FE;">แสดงความคิดเห็น</td></tr>
+          <tr><td><textarea name="message" rows="10" cols="30"></textarea></td></tr>
+          <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
+        </table>
+        <br>
+        <div align="center">
+            <a href="index.html">กลับไปหน้าหลัก</a>
+        </div>
+</body>
+</html>
